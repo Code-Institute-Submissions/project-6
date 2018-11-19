ab6fb32ec7c79b8bebeec47819ca08e524c4e96d
MiroslavSvec <vazkez.svec@gmail.com>
2018-11-19 01:58:48 +0000

    Fixing Heroku build

--- aph/settings.py
+++ aph/settings.py
@@ -23,10 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
+ALLOWED_HOSTS = ["127.0.0.1", "p5-aph.herokuapp.com"]
 
 
 # Application definition
